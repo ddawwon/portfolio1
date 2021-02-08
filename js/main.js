7f8a8b8e1f5e0801bc0766e61afa2a2150e2867b
ddawwon <76501372+ddawwon@users.noreply.github.com>
2021-02-08 18:15:19 +0900

    210208_1815

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    //네비 엑스버튼
+    var sta = 1 //상태1번: 팝업창이 안 열여있는 상태
+
+$('#nav_pop_btn').click(function(){
+  //  $(this).toggleClass('on');//단순 디자인 변경시에만 이용!! (on/off)꺼졌다 켜졌다 반복임,지금처럼 창이 같이 켜질때는 적절하지 않음.
+
+  if(sta == 1) {
+      $(this).addClass('on'); //엑스모양 버튼으로 보여짐=> on
+      $('#nav_pop').slideDown(); //박스가 보여야함
+
+      sta = 2; //상태2번: 상태가 바뀜
+
+  } else{ //상태2번 일때는 다 빼줘야함
+      $(this).removeClass('on');
+      $('#nav_pop').slideUp();
+      sta = 1; //원래 상태로 돌아옴
+  }
+})
+
+
+})//jq
---
+++ js/slide.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  $("#nav_pop.lazy").slick({
+    lazyLoad: 'ondemand', // ondemand progressive anticipated
+    arrows : false,
+    infinite: true,
+    autoplay : true,
+    autoplaySpeed : 1500,
+    speed : 1500,
+    pauseOnHover : true,
+    vertical : true,
+  });
+
+    $(".lazy").slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        arrows : false,
+        infinite: true,
+        autoplay : true,
+        autoplaySpeed : 1500,
+        speed : 1500,
+        pauseOnHover : true,
+        vertical : true,
+      });
+
+      $("#menu .lazy").slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        arrows : false,
+        infinite: true,
+        autoplay : true,
+        autoplaySpeed : 1500,
+        speed : 1500,
+        pauseOnHover : true,
+        vertical : true,
+      });
+      
+
+      
+})//jq
