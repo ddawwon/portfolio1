346e5c0a2af97c1daba15d3f7cdc9ba976c50f24
ddawwon <76501372+ddawwon@users.noreply.github.com>
2021-02-17 09:04:38 +0900

    210217_0904

--- js/sub2.js
+++ js/sub2.js
@@ -1,3 +1,27 @@
 $(function(){
-    
+
+//페럴렉스 텍스트
+let didScroll = false;
+let paralaxTitles = document.querySelectorAll('.paralax-title');
+
+const scrollInProgress = () => {
+  didScroll = true
+}
+
+const raf = () => {
+  if(didScroll) {
+    paralaxTitles.forEach((element, index) => {
+      element.style.transform = "translateX("+ window.scrollY / 10 + "%)"
+    })
+    didScroll = false;
+  }
+  requestAnimationFrame(raf);
+}
+
+
+requestAnimationFrame(raf);
+window.addEventListener('scroll', scrollInProgress)
+
+//
+
 })//jq
